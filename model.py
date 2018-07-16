6c46f0c8e4b21dc25d48b35c4288bad880e2616a
DarkHeroses <heri93@live.de>
2018-07-16 21:58:55 +0200

    https://www.youtube.com/watch?v=u8Z86g_gvEI

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pprint import pprint
 
+import winsound
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -56,8 +57,10 @@ def get_datasets_train():
     labels_tensor = tf.constant(np.array(labels))
 
     dataset = tf.data.Dataset.from_tensor_slices((filename_tensor, labels_tensor))
-    dataset = dataset.map(_parse_function)
-    dataset = dataset.batch(batch_size).repeat()
+
+    dataset = dataset.apply(tf.contrib.data.map_and_batch(map_func=_parse_function, batch_size=batch_size))
+
+    dataset = dataset.repeat()
     return dataset
 
 
@@ -134,7 +137,6 @@ def get_datasets_eval():
 
 
 def cnn_model_fn(features, labels, mode):
-    pprint(features)
     input_layer = tf.reshape(features, [-1, 64, 64, 3])
 
     conv1 = tf.layers.conv2d(
@@ -142,7 +144,7 @@ def cnn_model_fn(features, labels, mode):
         filters=30,
         kernel_size=[5, 5],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.tanh
     )
     maxp1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
@@ -151,7 +153,7 @@ def cnn_model_fn(features, labels, mode):
         filters=30,
         kernel_size=[4, 4],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.tanh
     )
     maxp2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
@@ -160,7 +162,7 @@ def cnn_model_fn(features, labels, mode):
         filters=30,
         kernel_size=[4, 4],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.tanh
     )
 
     maxp3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
@@ -170,7 +172,7 @@ def cnn_model_fn(features, labels, mode):
         filters=30,
         kernel_size=[3, 3],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.tanh
     )
 
     conv5 = tf.layers.conv2d(
@@ -178,30 +180,39 @@ def cnn_model_fn(features, labels, mode):
         filters=120,
         kernel_size=[3, 3],
         padding="same",
-        activation=tf.nn.relu
+        activation=tf.tanh
     )
 
     unit_count = 8 * 8 * 120
     flattened = tf.reshape(conv5, [-1, unit_count])
-    dense = tf.layers.dense(inputs=flattened, units=unit_count, activation=tf.nn.relu)
+    dense = tf.layers.dense(inputs=flattened, units=unit_count, activation=tf.tanh)
     dropout = tf.layers.dropout(
         inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-    full_layer2 = tf.layers.dense(inputs=dropout, units=84)
+    full_layer2 = tf.layers.dense(inputs=dropout, units=84, activation=tf.tanh)
     logits = tf.layers.dense(inputs=full_layer2, units=9)
 
     predictions = {'Predictions': tf.reshape(tensor=logits, shape=[-1, 3, 3], name="shaped_tensor")}
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
     loss = tf.losses.mean_squared_error(labels=labels, predictions=logits)
 
+    starter_learning_rate = 0.001
+    learning_rate = tf.train.exponential_decay(learning_rate=starter_learning_rate,
+                                               global_step=tf.train.get_global_step(),
+                                               decay_steps=10000,
+                                               decay_rate=0.1,
+                                               staircase=True)
+
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
         train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
     eval_metric_ops = {
-        "Root Mean Square Error": tf.metrics.root_mean_squared_error(labels=labels,
-                                                                     predictions=tf.cast(logits, dtype=tf.float64))}
+        "Mean Absolute Error": tf.metrics.mean_absolute_error(labels=labels,
+                                                              predictions=tf.cast(logits, dtype=tf.float64))}
 
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
@@ -220,12 +231,14 @@ def main(unused):
         print('Train the model')
         head_pose_classifier.train(
             input_fn=get_datasets_train,
-            max_steps=20000,
+            max_steps=40000,
             hooks=[logging_hook]
         )
+
         print("Evaluate the Model")
         eval_results = head_pose_classifier.evaluate(input_fn=get_datasets_eval)
         print(eval_results)
+        winsound.Beep(1500, 25)
     if len(sys.argv) == 2 and sys.argv[1].endswith(".png"):
         print("Predict the Rotation!")
         prediction = head_pose_classifier.predict(input_fn=get_predict_data)
